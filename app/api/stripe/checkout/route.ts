608b1aeca6144be78d54d63c4bc3373ca22cbd15
Sárközi Tamás <aiwithtomx@gmail.com>
2026-01-18 15:41:49 +0100

    stripe v0.2

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-12-15.clover",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: Request) {
   try {
